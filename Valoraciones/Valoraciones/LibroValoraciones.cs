b2d3f785aa941f393f6d72f01671ec15a22f303c
Esneider Serna <esneider.serna@doctus.com.co>
2018-01-03 18:01:56 -0500

    siguiendo con el curso

--- Valoraciones/Valoraciones/LibroValoraciones.cs
+++ Valoraciones/Valoraciones/LibroValoraciones.cs
@@ -8,11 +8,24 @@ namespace Valoraciones
 {
     class LibroValoraciones
     {
+
+        List<float> valoraciones;
+
+        public LibroValoraciones()
+        {
+            valoraciones = new List<float>();
+        }
+
+        public void PublicarValoraciones()
+        {
+            CalcularValoraciones calculo = new CalcularValoraciones();
+        }
+
         public void AgregarValoracion(float valoracion)
         {
             valoraciones.Add(valoracion);
         }
 
-        List<float> valoraciones;
+
     }
 }
--- Valoraciones/Valoraciones/Program.cs
+++ Valoraciones/Valoraciones/Program.cs
@@ -10,8 +10,20 @@ namespace Valoraciones
     {
         static void Main(string[] args)
         {
+            //Crear el objeto libro
             LibroValoraciones libro = new LibroValoraciones();
+
+            //Asignar valoraciones
             libro.AgregarValoracion(3.5f);
+            libro.AgregarValoracion(5);
+
+            //Publicar valoraciones
+            libro.PublicarValoraciones();
+
+            //ValoracionMinima
+            //ValoracionMaxima
+            //PromedioValoraciones
+
         }
     }
 }
